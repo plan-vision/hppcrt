9b953c2029979597e897a521f639b159617b6a49
vsonnier <vsonnier@users.noreply.github.com>
2014-03-13 14:34:36 +0100

    ArrayDeque.bufferIndexOf small tweak

--- hppc-core/src/main/templates/com/carrotsearch/hppc/KTypeArrayDeque.java
+++ hppc-core/src/main/templates/com/carrotsearch/hppc/KTypeArrayDeque.java
@@ -13,8 +13,8 @@ import static com.carrotsearch.hppc.Internals.*;
  * An array-backed deque (double-ended queue)  of KTypes. A single array is used to store and
  * manipulate all elements. Reallocations are governed by a {@link ArraySizingStrategy}
  * and may be expensive if they move around really large chunks of memory.
- * This dequeue is also a KTypeIndexedContainer, where index 0 is the head of the queue, and 
- * size() - 1 index is the last element. 
+ * This dequeue is also a KTypeIndexedContainer, where index 0 is the head of the queue, and
+ * size() - 1 index is the last element.
 #if ($TemplateOptions.KTypeGeneric)
  * A brief comparison of the API against the Java Collections framework:
  * <table class="nice" summary="Java Collections ArrayDeque and HPPC ObjectArrayDeque, related methods.">
@@ -49,7 +49,7 @@ import static com.carrotsearch.hppc.Internals.*;
  */
 /*! ${TemplateOptions.generatedAnnotation} !*/
 public class KTypeArrayDeque<KType>
-        extends AbstractKTypeCollection<KType> implements KTypeDeque<KType>, KTypeIndexedContainer<KType>, Cloneable
+extends AbstractKTypeCollection<KType> implements KTypeDeque<KType>, KTypeIndexedContainer<KType>, Cloneable
 {
     /**
      * Default capacity if no other capacity is given in the constructor.
@@ -382,7 +382,7 @@ public class KTypeArrayDeque<KType>
     /**
      * {@inheritDoc}
      * The returned position is relative to the head,
-     * i.e w.r.t the {@link KTypeIndexedContainer}, index 0 is the head of the queue, size() - 1 is the last element position. 
+     * i.e w.r.t the {@link KTypeIndexedContainer}, index 0 is the head of the queue, size() - 1 is the last element position.
      */
     @Override
     public int removeFirstOccurrence(final KType e1)
@@ -412,6 +412,7 @@ public class KTypeArrayDeque<KType>
     {
         final int last = tail;
         final int bufLen = buffer.length;
+        final KType[] buffer = this.buffer;
 
         for (int i = head; i != last; i = Intrinsics.oneRight(i, bufLen))
         {
@@ -425,7 +426,7 @@ public class KTypeArrayDeque<KType>
     /**
      * {@inheritDoc}
      * The returned position is relative to the head,
-     * i.e w.r.t the {@link KTypeIndexedContainer}, index 0 is the head of the queue, size() - 1 is the last element position. 
+     * i.e w.r.t the {@link KTypeIndexedContainer}, index 0 is the head of the queue, size() - 1 is the last element position.
      */
     @Override
     public int removeLastOccurrence(final KType e1)
@@ -473,7 +474,7 @@ public class KTypeArrayDeque<KType>
     /**
      * {@inheritDoc}
      * The returned position is relative to the head,
-     * i.e w.r.t the {@link KTypeIndexedContainer}, index 0 is the head of the queue, size() - 1 is the last element position. 
+     * i.e w.r.t the {@link KTypeIndexedContainer}, index 0 is the head of the queue, size() - 1 is the last element position.
      */
     @Override
     public int indexOf(final KType e1) {
@@ -484,7 +485,7 @@ public class KTypeArrayDeque<KType>
     /**
      * {@inheritDoc}
      * The returned position is relative to the head,
-     * i.e w.r.t the {@link KTypeIndexedContainer}, index 0 is the head of the queue, size() - 1 is the last element position. 
+     * i.e w.r.t the {@link KTypeIndexedContainer}, index 0 is the head of the queue, size() - 1 is the last element position.
      */
     @Override
     public int lastIndexOf(final KType e1) {
@@ -544,50 +545,50 @@ public class KTypeArrayDeque<KType>
                 : index >= head || index < tail) : "Index out of range (head="
                 + head + ", tail=" + tail + ", index=" + index + ").";
 
-        // Cache fields in locals (hopefully moved to registers).
-        final KType[] b = this.buffer;
-        final int bufLen = b.length;
-        final int lastIndex = bufLen - 1;
-        final int head = this.head;
-        final int tail = this.tail;
+                // Cache fields in locals (hopefully moved to registers).
+                final KType[] b = this.buffer;
+                final int bufLen = b.length;
+                final int lastIndex = bufLen - 1;
+                final int head = this.head;
+                final int tail = this.tail;
 
-        final int leftChunk = Math.abs(index - head) % bufLen;
-        final int rightChunk = Math.abs(tail - index) % bufLen;
+                final int leftChunk = Math.abs(index - head) % bufLen;
+                final int rightChunk = Math.abs(tail - index) % bufLen;
 
-        if (leftChunk < rightChunk)
-        {
-            if (index >= head)
-            {
-                System.arraycopy(b, head, b, head + 1, leftChunk);
-            }
-            else
-            {
-                System.arraycopy(b, 0, b, 1, index);
-                b[0] = b[lastIndex];
-                System.arraycopy(b, head, b, head + 1, lastIndex - head);
-            }
-            /*! #if ($TemplateOptions.KTypeGeneric) !*/
-            b[head] = Intrinsics.<KType> defaultKTypeValue();
-            /*! #end !*/
-            this.head = Intrinsics.oneRight(head, bufLen);
-        }
-        else
-        {
-            if (index < tail)
-            {
-                System.arraycopy(b, index + 1, b, index, rightChunk);
-            }
-            else
-            {
-                System.arraycopy(b, index + 1, b, index, lastIndex - index);
-                b[lastIndex] = b[0];
-                System.arraycopy(b, 1, b, 0, tail);
-            }
-            /*! #if ($TemplateOptions.KTypeGeneric) !*/
-            b[tail] = Intrinsics.<KType> defaultKTypeValue();
-            /*! #end !*/
-            this.tail = Intrinsics.oneLeft(tail, bufLen);
-        }
+                if (leftChunk < rightChunk)
+                {
+                    if (index >= head)
+                    {
+                        System.arraycopy(b, head, b, head + 1, leftChunk);
+                    }
+                    else
+                    {
+                        System.arraycopy(b, 0, b, 1, index);
+                        b[0] = b[lastIndex];
+                        System.arraycopy(b, head, b, head + 1, lastIndex - head);
+                    }
+                    /*! #if ($TemplateOptions.KTypeGeneric) !*/
+                    b[head] = Intrinsics.<KType> defaultKTypeValue();
+                    /*! #end !*/
+                    this.head = Intrinsics.oneRight(head, bufLen);
+                }
+                else
+                {
+                    if (index < tail)
+                    {
+                        System.arraycopy(b, index + 1, b, index, rightChunk);
+                    }
+                    else
+                    {
+                        System.arraycopy(b, index + 1, b, index, lastIndex - index);
+                        b[lastIndex] = b[0];
+                        System.arraycopy(b, 1, b, 0, tail);
+                    }
+                    /*! #if ($TemplateOptions.KTypeGeneric) !*/
+                    b[tail] = Intrinsics.<KType> defaultKTypeValue();
+                    /*! #end !*/
+                    this.tail = Intrinsics.oneLeft(tail, bufLen);
+                }
     }
 
     /**
@@ -1135,7 +1136,7 @@ public class KTypeArrayDeque<KType>
      * instead of using a constructor).
      */
     public static/* #if ($TemplateOptions.KTypeGeneric) */<KType> /* #end */
-            KTypeArrayDeque<KType> newInstance()
+    KTypeArrayDeque<KType> newInstance()
     {
         return new KTypeArrayDeque<KType>();
     }
@@ -1145,7 +1146,7 @@ public class KTypeArrayDeque<KType>
      * instead of using a constructor).
      */
     public static/* #if ($TemplateOptions.KTypeGeneric) */<KType> /* #end */
-            KTypeArrayDeque<KType> newInstanceWithCapacity(final int initialCapacity)
+    KTypeArrayDeque<KType> newInstanceWithCapacity(final int initialCapacity)
     {
         return new KTypeArrayDeque<KType>(initialCapacity);
     }
@@ -1154,7 +1155,7 @@ public class KTypeArrayDeque<KType>
      * Create a new deque by pushing a variable number of arguments to the end of it.
      */
     public static/* #if ($TemplateOptions.KTypeGeneric) */<KType> /* #end */
-            KTypeArrayDeque<KType> from(final KType... elements)
+    KTypeArrayDeque<KType> from(final KType... elements)
     {
         final KTypeArrayDeque<KType> coll = new KTypeArrayDeque<KType>(elements.length);
         coll.addLast(elements);
@@ -1165,7 +1166,7 @@ public class KTypeArrayDeque<KType>
      * Create a new deque by pushing a variable number of arguments to the end of it.
      */
     public static/* #if ($TemplateOptions.KTypeGeneric) */<KType> /* #end */
-            KTypeArrayDeque<KType> from(final KTypeArrayDeque<KType> container)
+    KTypeArrayDeque<KType> from(final KTypeArrayDeque<KType> container)
     {
         return new KTypeArrayDeque<KType>(container);
     }
@@ -1236,7 +1237,7 @@ public class KTypeArrayDeque<KType>
     /**
      * {@inheritDoc}
      * The position is relative to the head,
-     * i.e w.r.t the {@link KTypeIndexedContainer}, index 0 is the head of the queue, size() - 1 is the last element position. 
+     * i.e w.r.t the {@link KTypeIndexedContainer}, index 0 is the head of the queue, size() - 1 is the last element position.
      */
     @Override
     public KType set(final int index, final KType e1) {
@@ -1253,7 +1254,7 @@ public class KTypeArrayDeque<KType>
     /**
      * {@inheritDoc}
      * The position is relative to the head,
-     * i.e w.r.t the {@link KTypeIndexedContainer}, index 0 is the head of the queue, size() - 1 is the last element position. 
+     * i.e w.r.t the {@link KTypeIndexedContainer}, index 0 is the head of the queue, size() - 1 is the last element position.
      */
     @Override
     public KType get(final int index) {
@@ -1264,7 +1265,7 @@ public class KTypeArrayDeque<KType>
     /**
      * {@inheritDoc}
      * The position is relative to the head,
-     * i.e w.r.t the {@link KTypeIndexedContainer}, index 0 is the head of the queue, size() - 1 is the last element position. 
+     * i.e w.r.t the {@link KTypeIndexedContainer}, index 0 is the head of the queue, size() - 1 is the last element position.
      */
     @Override
     public KType remove(final int index) {
