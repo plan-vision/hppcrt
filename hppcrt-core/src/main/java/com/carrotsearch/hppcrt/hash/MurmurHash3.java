1a836149c5da98f7489097a8076dbc26d28a3019
vsonnier <vsonnier@gmail.com>
2015-01-17 10:22:17 +0100

    Updated deps, small MurmurHash3 docs changes

--- hppcrt-core/src/main/java/com/carrotsearch/hppcrt/hash/MurmurHash3.java
+++ hppcrt-core/src/main/java/com/carrotsearch/hppcrt/hash/MurmurHash3.java
@@ -8,14 +8,13 @@ package com.carrotsearch.hppcrt.hash;
  */
 public final class MurmurHash3
 {
-
     /**
-     * = MurmurHash3.hash(0)
+     * = hash(0)
      */
     public static final int HASH_0 = 0;
 
     /**
-     * = MurmurHash3.hash(1)
+     * = hash(1)
      */
     public static final int HASH_1 = 1364076727;
 
@@ -26,6 +25,8 @@ public final class MurmurHash3
 
     /**
      * Hashes a 4-byte sequence (Java int).
+     * @param x an integer.
+     * @return a hash value obtained by mixing the bits of {@code x}.
      */
     public static int hash(int k)
     {
@@ -39,6 +40,8 @@ public final class MurmurHash3
 
     /**
      * Hashes an 8-byte sequence (Java long).
+     * @param x a long integer.
+     * @return a hash value obtained by mixing the bits of {@code x}.
      */
     public static long hash(long k)
     {
