b399c6b0310ee7f6fa80ffe92be4f936f565d4ea
vsonnier <vsonnier@gmail.com>
2015-06-09 11:27:53 +0200

    Factorize ArrayDeque tests as KTypeDeque tests

--- hppcrt/src/test/templates/com/carrotsearch/hppcrt/lists/AbstractKTypeIndexedContainerTest.java
+++ hppcrt/src/test/templates/com/carrotsearch/hppcrt/lists/AbstractKTypeIndexedContainerTest.java
@@ -19,7 +19,7 @@ import com.carrotsearch.randomizedtesting.annotations.*;
 
 /*! #import("com/carrotsearch/hppcrt/Intrinsics.java") !*/
 /**
- * Tests common for all kinds of hash sets {@link KTypeIndexedContainer}.
+ * Tests common for all kinds of {@link KTypeIndexedContainer}.
  */
 // ${TemplateOptions.doNotGenerateKType("BOOLEAN")}
 /*! ${TemplateOptions.generatedAnnotation} !*/
@@ -307,13 +307,13 @@ public abstract class AbstractKTypeIndexedContainerTest<KType> extends AbstractK
         addFromArray(this.list, newArray(this.k0, this.k1, this.k2, this.k1, this.k4));
 
         Assert.assertEquals(3, this.list.removeAll(new KTypePredicate<KType>()
-        {
+                {
             @Override
             public boolean apply(final KType v)
             {
                 return v == AbstractKTypeIndexedContainerTest.this.key1 || v == AbstractKTypeIndexedContainerTest.this.key2;
             };
-        }));
+                }));
 
         TestUtils.assertListEquals(this.list.toArray(), 0, 4);
     }
@@ -325,25 +325,25 @@ public abstract class AbstractKTypeIndexedContainerTest<KType> extends AbstractK
         addFromArray(this.list, newArray(this.k0, this.k1, this.k2, this.k1, this.k4));
 
         Assert.assertEquals(5, this.list.removeAll(new KTypePredicate<KType>()
-        {
+                {
             @Override
             public boolean apply(final KType v)
             {
                 return true;
             };
-        }));
+                }));
 
         Assert.assertEquals(0, this.list.size());
 
         //try again
         Assert.assertEquals(0, this.list.removeAll(new KTypePredicate<KType>()
-        {
+                {
             @Override
             public boolean apply(final KType v)
             {
                 return true;
             };
-        }));
+                }));
 
         Assert.assertEquals(0, this.list.size());
     }
@@ -355,13 +355,13 @@ public abstract class AbstractKTypeIndexedContainerTest<KType> extends AbstractK
         addFromArray(this.list, newArray(this.k0, this.k1, this.k2, this.k1, this.k0));
 
         Assert.assertEquals(2, this.list.retainAll(new KTypePredicate<KType>()
-        {
+                {
             @Override
             public boolean apply(final KType v)
             {
                 return v == AbstractKTypeIndexedContainerTest.this.key1 || v == AbstractKTypeIndexedContainerTest.this.key2;
             };
-        }));
+                }));
 
         TestUtils.assertListEquals(this.list.toArray(), 1, 2, 1);
     }
@@ -378,7 +378,7 @@ public abstract class AbstractKTypeIndexedContainerTest<KType> extends AbstractK
             //the assert below should never be triggered because of the exception
             //so give it an invalid value in case the thing terminates  = initial size
             Assert.assertEquals(5, this.list.removeAll(new KTypePredicate<KType>()
-            {
+                    {
                 @Override
                 public boolean apply(final KType v)
                 {
@@ -387,7 +387,7 @@ public abstract class AbstractKTypeIndexedContainerTest<KType> extends AbstractK
                     }
                     return v == AbstractKTypeIndexedContainerTest.this.key1;
                 };
-            }));
+                    }));
             Assert.fail();
         } catch (final RuntimeException e)
         {
---
+++ hppcrt/src/test/templates/com/carrotsearch/hppcrt/lists/KTypeArrayDequeAsDequeTest.java
@@ -0,0 +1,73 @@
+package com.carrotsearch.hppcrt.lists;
+
+import org.junit.After;
+import org.junit.Assert;
+
+import com.carrotsearch.hppcrt.Intrinsics;
+import com.carrotsearch.hppcrt.KTypeContainer;
+
+public class KTypeArrayDequeAsDequeTest<KType> extends AbstractKTypeDequeTest<KType>
+{
+    @Override
+    protected KTypeDeque<KType> createNewInstance(final int initialCapacity) {
+
+        return new KTypeArrayDeque<KType>(initialCapacity);
+    }
+
+    @Override
+    protected void addFromArray(final KTypeDeque<KType> testList, final KType... keys) {
+        final KTypeArrayDeque<KType> concreteClass = (KTypeArrayDeque<KType>) (testList);
+        concreteClass.addLast(keys);
+
+    }
+
+    @Override
+    protected void addFromContainer(final KTypeDeque<KType> testList, final KTypeContainer<KType> container) {
+        final KTypeArrayDeque<KType> concreteClass = (KTypeArrayDeque<KType>) (testList);
+        concreteClass.addLast(container);
+
+    }
+
+    @Override
+    protected KType[] getBuffer(final KTypeDeque<KType> testList) {
+        final KTypeArrayDeque<KType> concreteClass = (KTypeArrayDeque<KType>) (testList);
+        return Intrinsics.<KType[]> cast(concreteClass.buffer);
+    }
+
+    @Override
+    int getDescendingValuePoolSize(final KTypeDeque<KType> testList) {
+        final KTypeArrayDeque<KType> concreteClass = (KTypeArrayDeque<KType>) (testList);
+        return concreteClass.descendingValueIteratorPool.size();
+    }
+
+    @Override
+    int getDescendingValuePoolCapacity(final KTypeDeque<KType> testList) {
+        final KTypeArrayDeque<KType> concreteClass = (KTypeArrayDeque<KType>) (testList);
+        return concreteClass.descendingValueIteratorPool.capacity();
+    }
+
+    /**
+     * Move one index to the right, wrapping around buffer of size modulus
+     */
+    private int oneRight(final int index, final int modulus)
+    {
+        return (index + 1 == modulus) ? 0 : index + 1;
+    }
+
+    @After
+    public void checkConsistency()
+    {
+
+        final KTypeArrayDeque<KType> arrayDeque = (KTypeArrayDeque<KType>) this.deque;
+
+        if (this.deque != null)
+        {
+            for (int i = arrayDeque.tail; i < arrayDeque.head; i = oneRight(i, getBuffer(this.deque).length))
+            {
+                /*! #if ($TemplateOptions.KTypeGeneric) !*/
+                Assert.assertTrue(Intrinsics.<KType> empty() == getBuffer(this.deque)[i]);
+                /*! #end !*/
+            }
+        }
+    }
+}
